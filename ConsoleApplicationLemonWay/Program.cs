01d0dac1bd08803cdbb74b4ef55d86c0fceb8b76
dgieng <daniel.gieng@gmail.com>
2016-05-25 23:41:48 +0200

    FirstCommit

---
+++ ConsoleApplicationLemonWay/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplicationLemonWay
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (WSLemonWay.Service ws = new WSLemonWay.Service())
+            {
+                int result = ws.Fibonacci(10);
+                Console.WriteLine("Le résultat de Fibonnaci(10) est " + result);
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ UnitTestLemonWay/UnitTest1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestLemonWay
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestFibonnaci()
+        {
+            using (WSLemonWay.Service ws = new WSLemonWay.Service())
+            {
+                int fibN = 10;
+                int result = 0;
+                int resultExpected = 55;
+
+                result = ws.Fibonacci(fibN);
+                Assert.AreEqual(result, resultExpected, "La suite de Fibonacci de rang 10 est correcte");
+            }
+        }
+    }
+}
